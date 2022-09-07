6f99edcb0e7dcfcdb101c23a5d5131b55ce64a79
sinmunsu <ace05227@gmail.com>
2022-09-07 20:23:54 +0900

    1. WordCRUD 클래스와 인터페이스 생성 2. 단어 추가 기능 구현 3. 모든 단어 보기 기능 추가 구현

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -27,13 +27,15 @@ public class WordManager {
     public void start() {
         while (true) {
             int menu = selectMenu();
-            System.out.println(menu);
-            if(menu == 0) break;
+            if(menu == 0) {
+                System.out.print("프로그램 종료! 다음에 만나요~ ");
+                break;
+            }
             if(menu == 4){
                 wordCRUD.addWord();
             }
             else if(menu == 1){
-
+                wordCRUD.listAll();
             }
         }
     }
