409cf9b9a8bc87dd1f9daeae2c7bb29a3c8ad8a0
sinmunsu <ace05227@gmail.com>
2022-09-17 18:50:15 +0900

    5번 단어 수정 기능 구현 완료

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -37,6 +37,12 @@ public class WordManager {
             else if(menu == 1){
                 wordCRUD.listAll();
             }
+            else if(menu == 5){
+                wordCRUD.updateItem();
+            }
+            else if(menu == 6){
+
+            }
         }
     }
 }
